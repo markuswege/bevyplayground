1f91f28775e104650608c7b49c9ded21229c8f6b
Markus Wege <markus.wege-extern@deutschebahn.com>
2024-06-26 09:33:51 +0200

    FPS display added for fun

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,27 @@
+use bevy::prelude::*;
+use iyes_perf_ui::{PerfUiPlugin, PerfUiRoot};
+use iyes_perf_ui::diagnostics::PerfUiEntryFPS;
+use iyes_perf_ui::prelude::PerfUiEntryFPSWorst;
+
 fn main() {
-    println!("Hello, world!");
+    App::new()
+        .add_plugins(DefaultPlugins)
+        .add_plugins(bevy::diagnostic::FrameTimeDiagnosticsPlugin)
+        .add_plugins(PerfUiPlugin)
+        .add_systems(Startup, setup)
+        .run();
+}
+
+fn setup(mut commands: Commands) {
+    commands.spawn(Camera2dBundle::default());
+
+    commands.spawn((
+        PerfUiRoot {
+            display_labels: true,
+            layout_horizontal: false,
+            ..default()
+        },
+        PerfUiEntryFPSWorst::default(),
+        PerfUiEntryFPS::default(),
+    ));
 }
